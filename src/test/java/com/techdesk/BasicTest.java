cec9d88dc390d3e3a34c96b09ba25c28b7a4387f
Mehdi <elmehdiworkemail@gmail.com>
2025-02-22 13:45:49 +0000

    users tests created

---
+++ src/test/java/com/techdesk/BasicTest.java
@@ -0,0 +1,12 @@
+package com.techdesk;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicTest {
+
+    @Test
+    void simpleTest() {
+        assertTrue(true, "This test should always pass");
+    }
+}
